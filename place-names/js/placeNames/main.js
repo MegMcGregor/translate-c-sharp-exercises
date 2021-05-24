ec1c9291f57d990ed2b0d3f0e1ac87bcfb8057d2
Meg McGregor <megmcgregor1@gmail.com>
2021-05-24 12:09:41 -0700

    place names complete

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,14 @@
 // Put your code here
+
+let cities = ["Nashville", "Hong Kong", "The back yard", "Earth",
+"London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log(cities);
+
+let theCities = cities.filter(city => {
+    if (city.toLowerCase().includes("the")) {
+        return true
+    }
+})
+
+console.log(theCities);
