d39757f3ca778ccdb35bc0fb580d5d4c8473801a
Meg McGregor <megmcgregor1@gmail.com>
2021-05-25 09:18:48 -0700

    completed rolling dice

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,21 @@
 // Put your code here
+console.log("Let's roll some dice, baby!")
+console.log("-----------------------------")
+
+
+let roll = () => Math.floor(Math.random() * (6 - 1) + 1);
+
+
+for (let i = 0; i < 10; i++) {
+
+    let die1 = roll();
+    let die2 = roll();
+
+    let result = `${die1} + ${die2}`;
+
+    if (die1 == die2) {
+        console.log(result + " = DOUBLES!")
+    } else {
+        console.log(result + " = " + `${die1 + die2}`)
+    }
+}
